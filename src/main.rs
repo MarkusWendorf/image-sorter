feccd5f3e803946a08c29ffcde038d0f1bd085e3
Markus Wendorf <markuswendorf@web.de>
2025-03-01 17:33:21 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(let_chains)]
+
 use std::{
     fs, io,
     path::{Path, PathBuf},
@@ -29,6 +31,11 @@ fn main() {
     let source_dirs = args.folders;
     let dest_dir = args.out;
 
+    let data_dir = Path::new(&dest_dir);
+    fs::create_dir_all(data_dir).expect("failed to create output directory");
+
+    let default_timezone_offset = "+02:00".to_owned(); // todo: cli arg
+
     let images = source_dirs
         .iter()
         .flat_map(|dir| fs::read_dir(dir).unwrap())
@@ -42,7 +49,7 @@ fn main() {
 
             let image = ImageFile {
                 name: file.file_name().into_string().unwrap(),
-                date_time: get_date_time(path),
+                date_time: get_date_time(path, default_timezone_offset.clone()),
                 path: file.path(),
             };
 
@@ -56,7 +63,7 @@ fn main() {
     }
 }
 
-fn get_date_time(file_path: &Path) -> String {
+fn get_date_time(file_path: &Path, default_timezone_offset: String) -> String {
     let file = fs::File::open(file_path).unwrap();
     let mut reader = io::BufReader::new(&file);
 
@@ -64,11 +71,24 @@ fn get_date_time(file_path: &Path) -> String {
         .read_from_container(&mut reader)
         .unwrap();
 
-    let exif_date_time = get_exif_field(&exif, Tag::DateTimeDigitized);
-    let exif_time_offset = get_exif_field(&exif, Tag::OffsetTimeOriginal);
+    let date_time = get_next_best_exif_field(
+        &exif,
+        &[Tag::DateTimeDigitized, Tag::DateTimeOriginal, Tag::DateTime],
+    );
 
-    let offset = parse_offset(&exif_time_offset);
-    parse_date_time(&exif_date_time, offset)
+    let time_offset = get_next_best_exif_field(
+        &exif,
+        &[
+            Tag::OffsetTimeDigitized,
+            Tag::OffsetTimeOriginal,
+            Tag::OffsetTime,
+        ],
+    );
+
+    parse_date_time(
+        &date_time.expect("no date time metadata found"),
+        parse_offset(&time_offset.unwrap_or(default_timezone_offset)),
+    )
 }
 
 fn parse_offset(offset_str: &str) -> &str {
@@ -76,6 +96,7 @@ fn parse_offset(offset_str: &str) -> &str {
 }
 
 fn parse_date_time(date_time_str: &str, offset: &str) -> String {
+    println!("{}, {}", date_time_str, offset);
     DateTime::parse_from_str(&format!("{} {}", date_time_str, offset), "%F %T %:z")
         .unwrap()
         .to_utc()
@@ -83,10 +104,17 @@ fn parse_date_time(date_time_str: &str, offset: &str) -> String {
         .to_string()
 }
 
-fn get_exif_field(exif: &Exif, tag: Tag) -> String {
+fn get_exif_field(exif: &Exif, tag: Tag) -> Option<String> {
     exif.get_field(tag, In::PRIMARY)
-        .unwrap()
-        .value
-        .display_as(tag)
-        .to_string()
+        .map(|field| field.value.display_as(tag).to_string())
+}
+
+fn get_next_best_exif_field(exif: &Exif, tags: &[Tag]) -> Option<String> {
+    for &tag in tags {
+        if let Some(field) = get_exif_field(exif, tag) {
+            return Some(field);
+        }
+    }
+
+    return None;
 }
